d2f5f059023aef9b719b5064e3c908eadc9d996e
John Power <superpozitiv71@gmail.com>
2020-03-23 17:47:56 +0700

    done 90%

--- script.js
+++ script.js
@@ -24,6 +24,47 @@ function createProgram(gl, vertexShader, fragmentShader) {
     console.log(gl.getProgramInfoLog(program));
     gl.deleteProgram(program);
 }
+
+const rotate = (fi) => {
+    return [
+        [Math.cos(fi), Math.sin(fi), 0],
+        [-Math.sin(fi), Math.cos(fi), 0],
+        [0 , 0 , 1]
+    ];
+}
+
+const scale = (a, b) => {
+    return [
+        [a, 0, 0],
+        [0, b, 0],
+        [0, 0, 1]
+    ];
+}
+
+const invert = () => { 
+    return [
+        [1, 0, 0],
+        [0, -1, 0],
+        [0, 0, 1]
+    ];
+}
+
+const translate = (a, b) => {
+    return [
+        [1, 0, 0],
+        [0, 1, 0],
+        [a, b, 1]
+    ];
+}
+const identity = () => {
+    return [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ];
+}
+
+
 function start() {
     var canvas = document.getElementById("glcanvas");
     var gl = canvas.getContext("webgl");    // инициализация контекста GL
@@ -31,6 +72,8 @@ function start() {
     if (!gl) {
         console.log("webGL doesnt work")
     }
+
+    
 
     var vertexShaderSource = document.getElementById("2d-vertex-shader").text;
     var fragmentShaderSource = document.getElementById("2d-fragment-shader").text;
@@ -46,7 +89,7 @@ function start() {
     gl.enable(gl.DEPTH_TEST);                               // включает использование буфера глубины
     gl.depthFunc(gl.LEQUAL);                                // определяет работу буфера глубины: более ближние объекты перекрывают дальние
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.clearColor(0, 0, 0, 0);
+    gl.clearColor(0, 0, 0,1);
     
     var verticies = [0.0, 0.5, -0.5,-0.5, 0.5,-0.5];
   
@@ -61,12 +104,81 @@ function start() {
         figure[i] /= 8.0; //normalize
     }
     console.log(figure);
+    let uniformTranslateMatLocation = gl.getUniformLocation(program,"u_translate");
+    let uniformScaleMatLocation = gl.getUniformLocation(program,"u_scale");
+    let uniformInvertMatLocation = gl.getUniformLocation(program,"u_invert");
+    let uniformRotateMatLocation = gl.getUniformLocation(program,"u_rotate");
 
+
+    gl.uniformMatrix3fv(uniformTranslateMatLocation,false,identity().flat());
+    gl.uniformMatrix3fv(uniformScaleMatLocation,false,identity().flat());
+    gl.uniformMatrix3fv(uniformInvertMatLocation,false,identity().flat());
+    gl.uniformMatrix3fv(uniformRotateMatLocation,false,identity().flat());
+
+
+    let translateSliderX = document.getElementById("translateSliderX");
+    let translateSliderY = document.getElementById("translateSliderY");
+    let scaleSlider = document.getElementById("scaleSlider");
+    let rotateSlider = document.getElementById("rotateSlider");
+    let invertCheckBox = document.getElementById("invertCheckBox");
+
+    let inverted = false;
+    invertCheckBox.onchange = () => {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clearColor(0, 0, 0,1);
+
+        if(inverted){
+            gl.uniformMatrix3fv(uniformInvertMatLocation,false,identity().flat());
+        }else{
+            gl.uniformMatrix3fv(uniformInvertMatLocation,false,invert().flat());
+        }
+        gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.drawElements(gl.LINES , 10, gl.UNSIGNED_SHORT, 0);
+        inverted = !inverted;
+    }
+
+    rotateSlider.oninput = () => {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clearColor(0, 0, 0,1);
+
+        gl.uniformMatrix3fv(uniformRotateMatLocation,false,rotate(rotateSlider.value).flat());
+        gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.drawElements(gl.LINES , 10, gl.UNSIGNED_SHORT, 0);
+    }
+
+    scaleSlider.oninput = () => {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clearColor(0, 0, 0,1);
+        
+        gl.uniformMatrix3fv(uniformScaleMatLocation,false,scale(scaleSlider.value,scaleSlider.value).flat());
+        gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.drawElements(gl.LINES , 10, gl.UNSIGNED_SHORT, 0);
+    }
+
+    translateSliderY.oninput = () => {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clearColor(0, 0, 0,1);
+        
+        
+        gl.uniformMatrix3fv(uniformTranslateMatLocation,false,translate(translateSliderX.value,translateSliderY.value).flat());
+        gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.drawElements(gl.LINES , 10, gl.UNSIGNED_SHORT, 0);
+    }
+    translateSliderX.oninput = () => {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clearColor(0, 0, 0,1);
+        
+        gl.uniformMatrix3fv(uniformTranslateMatLocation,false,translate(translateSliderX.value,translateSliderY.value).flat());
+        gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.drawElements(gl.LINES , 10, gl.UNSIGNED_SHORT, 0);
+    }
+        
     
-
-
-    
-
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
     gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(figure),gl.STATIC_DRAW);
